9beb92d0db7759c1710cce45e0143c7ad1e2788a
Rakin Rahman <rakinhacker@gmail.com>
2022-08-26 16:49:32 +0600

    Rename file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from './components/Greet.vue';
+import Calculate from './components/Calculate.vue';
 </script>
 
 <template>
     <div class="container">
-        <Greet />
+        <Calculate />
     </div>
 </template>
 
